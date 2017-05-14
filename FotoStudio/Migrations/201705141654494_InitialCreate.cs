198359a24b69931079e156d0600c840fcbe5bb60
HectorR02 <engel-t@outlook.es>
2017-05-14 13:28:57 -0400

    DB, Migrations

---
+++ FotoStudio/Migrations/201705141654494_InitialCreate.cs
@@ -0,0 +1,40 @@
+namespace FotoStudio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TipoUsuarios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Usuarios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FullName = c.String(nullable: false, maxLength: 100),
+                        UserName = c.String(nullable: false, maxLength: 12),
+                        Email = c.String(nullable: false),
+                        Contraseña = c.String(nullable: false, maxLength: 15),
+                        Tipo = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Usuarios");
+            DropTable("dbo.TipoUsuarios");
+        }
+    }
+}
