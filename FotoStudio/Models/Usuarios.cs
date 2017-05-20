c4f6433c6375a812a92dc059c244c692f4a8271e
HectorR02 <engel-t@outlook.es>
2017-05-20 10:19:23 -0400

    Validacion -Solo letras -Letras y numeros

--- FotoStudio/Models/Usuarios.cs
+++ FotoStudio/Models/Usuarios.cs
@@ -13,23 +13,26 @@ namespace FotoStudio.Models
         [Key]
         public int Id { get; set; }
         
-        [Display(Name = "Nombre Completo"), Required(ErrorMessage = "*Este campo es obligatorio*")]
+        [Display(Name = "Nombre Completo"), Required(ErrorMessage = "*Este campo es obligatorio")]
         [StringLength(100, ErrorMessage = "La longitud del {0} no debe ser menor de {2} caractéres", MinimumLength = 5)]
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string FullName { get; set; }
 
-        [Display(Name = "Nombre de Usuario"), Required(ErrorMessage = "*Este campo es obligatorio*")]
+        [Display(Name = "Nombre de Usuario"), Required(ErrorMessage = "*Este campo es obligatorio")]
         [StringLength(12, ErrorMessage = "La longitud del {0} no debe ser menor de {2} caractéres", MinimumLength = 5)]
+        [RegularExpression(@"^[0-9a-zA-Z]*")]
         public string UserName { get; set; }
 
-        [EmailAddress]
-        [Display(Name = "Correo Electrónico"), Required(ErrorMessage = "*Este campo es obligatorio*")]
+        [EmailAddress(ErrorMessage = "El correo no tiene el formato correcto")]
+        [Display(Name = "Correo Electrónico"), Required(ErrorMessage = "*Este campo es obligatorio")]
         public string Email { get; set; }
 
-        [DataType(DataType.Password), Required(ErrorMessage = "*Este campo es obligatorio*")]
+        [DataType(DataType.Password), Required(ErrorMessage = "*Este campo es obligatorio")]
         [StringLength(15, ErrorMessage = "La longitud del {0} no debe ser menor de {2} caractéres", MinimumLength = 8)]
         public string Contraseña { get; set; }
 
-        [Display(Name = "Tipo de Usuario"), Required(ErrorMessage = "*Este campo es obligatorio*")]
+        [Display(Name = "Tipo de Usuario"), Required(ErrorMessage = "*Este campo es obligatorio")]
         public int Tipo { get; set; }
     }
 }
